72ed876460d7ae0a586b89bc3df9e80ffc1fbc8a
Himanshu Jadon <96410955+himanshu-957@users.noreply.github.com>
2023-02-28 14:23:59 +0530

    Delete celcius.c

--- celcius.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
- int farh;
- printf("Enter the temperature in Fahrenheit: ");
- scanf("%d", &farh);
- float celc = (farh - 32)*5/9;
- printf("The temperature in celcius is: %f", celc);
-}
